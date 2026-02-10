31b6850524a0d20c3b8e9e57c8249d33281f3619
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-02-10 16:54:51 +0530

    SMTP port change

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -2,16 +2,13 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
-    secure: false, // use TLS
+    port: 465,
+    secure: true, // use SSL
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
-    tls:{
-        rejectUnauthorized:false,
-    },
-    connectionTimeout: 20000, // 10 seconds
+    connectionTimeout: 20000,
     greetingTimeout: 20000,
     socketTimeout: 20000,
 });
