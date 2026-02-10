078e1ca9aec094eb17d378485bb8201ec2f923dd
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-02-10 15:19:56 +0530

    error fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
@@ -6,9 +6,9 @@ import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>,
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>,
 )
