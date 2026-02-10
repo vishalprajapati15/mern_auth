7b8a0a33e16e9c569f77c701ae82009cca446919
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-02-10 15:55:36 +0530

    Error fix

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -3,10 +3,14 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
     port: 587,
+    secure: false, // use TLS
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 });
 
 export default transporter;
